2c5028f7635b25470487ec1003d99504d8ea6bd1
Eddie Lin <elin@mozilla.com>
2013-05-24 08:26:51 +0800

    Parse nightly version on update_product_details.

--- apps/mocotw/utils.py
+++ apps/mocotw/utils.py
@@ -1,5 +1,22 @@
+import json
+import logging
+import re
+from product_details import settings_fallback, product_details
+
+
+log = logging.getLogger('prod_details')
+version_pattern = re.compile('firefox\-([a-z0-9\.]+)\.en\-US')
+nightly_file = '/firefox_nightly_version.json'
+nightly_key = 'LATEST_NIGHTLY_VERSION'
+
+
 def latest_nightly_version(locale):
-    return '24.0a1', []
+    try:
+        nightly_version = product_details.firefox_nightly_version[nightly_key]
+    except KeyError:
+        log.error('No version key found.')
+        nightly_version = '24.0a1'
+    return nightly_version, []
 
 
 def make_nightly_mobile_link(version):
@@ -18,4 +35,24 @@ def make_nightly_link(product, version, platform, locale):
     return ('%s/%s-%s.%s.%s' %
             ('https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk',
              product, version, locale, filename))
+
+
+def download_nightly_details():
+    """
+    parse latest nightly version from ftp page
+    >>> download_nightly_details(nightly_file)
+    """
+    import urllib2
+    response = urllib2.urlopen('https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/').read()
+    version = version_pattern.search(response).group(1)
+    try:
+        PROD_DETAILS_DIR = settings_fallback('PROD_DETAILS_DIR')
+        f = open(PROD_DETAILS_DIR + nightly_file, 'w')
+        try:
+            f.write('{"%s":"%s"}' % (nightly_key, version))
+        finally:
+            f.close()
+            log.info('Written newest nightly version in %s' % nightly_file)
+    except IOError:
+        log.error('Failed to write nightly version file.')
 
