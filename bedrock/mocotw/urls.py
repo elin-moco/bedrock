15467de73c6219a9eab6e625edaf30626089d252
Eddie Lin <elin@mozilla.com>
2013-07-23 17:28:24 +0800

    Added phishing protection page.

--- bedrock/mocotw/urls.py
+++ bedrock/mocotw/urls.py
@@ -3,12 +3,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from django.conf.urls.defaults import *
+from bedrock.mozorg.util import page
 from bedrock.mozorg.views import contribute
 from bedrock.redirects.util import redirect
 
 urlpatterns = patterns(
     '',
     redirect(r'^firefox/mobile/$', '/firefox/mobile/features'),
+    page('firefox/phishing-protection', 'firefox/phishing-protection.html'),
     url('^community/contribute/$', contribute, name='mozorg.contribute',
         kwargs={'template': 'mozorg/contribute.html',
                 'return_to_form': False}),
