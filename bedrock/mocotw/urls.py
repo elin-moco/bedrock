55d278ce51e72f8031d719d1ac999a0b474c85c5
Eddie Lin <elin@mozilla.com>
2013-08-23 14:08:02 +0800

    Redirect press urls.

--- bedrock/mocotw/urls.py
+++ bedrock/mocotw/urls.py
@@ -9,6 +9,7 @@ from bedrock.mozorg.views import contribute
 from bedrock.mozorg.views import contact_bizdev
 from bedrock.mozorg.views import plugincheck
 from bedrock.redirects.util import redirect
+from bedrock.sandstone.settings import BLOG_URL
 
 urlpatterns = patterns(
     '',
@@ -25,6 +26,8 @@ urlpatterns = patterns(
     page('firefox/ueip', 'firefox/ueip.html'),
     page('firefox/mobile/sync', 'firefox/mobile/sync.html'),
     page('firefox/phishing-protection', 'firefox/phishing-protection.html'),
+    redirect(r'^press/$', 'http://' + BLOG_URL + '/press'),
+    redirect(r'^news/press/$', 'http://' + BLOG_URL + '/press'),
     redirect(r'^firefoxos/$', '/firefox/os'),
     redirect(r'^firefox/central/$', '/firefox/features'),
     redirect(r'^firefox/mobile/$', '/firefox/mobile/features'),
