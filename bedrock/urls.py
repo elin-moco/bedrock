25ded98afd8d1acbd2c82eeaec470a1bc2959e2e
Eddie Lin <elin@mozilla.com>
2013-09-09 18:02:39 +0800

    Fixed sitemap freq.

--- bedrock/urls.py
+++ bedrock/urls.py
@@ -38,7 +38,7 @@ sitemaps = {
     'about': ViewsSitemap(
         ['mozorg.about', 'mocotw.about.manifesto', 'mocotw.about.space',
          'mocotw.about.careers', 'mocotw.about.intern', 'mocotw.about.contact'],
-        0.6, 'montyly'),
+        0.6, 'monthly'),
     'community': ViewsSitemap(
         ['mozorg.contribute', 'mocotw.community.student.workshop'],
         0.5, 'monthly'),
