372dc0564ad50ec51ed8a7568d69a4905867acc5
Eddie Lin <elin@mozilla.com>
2013-10-30 18:19:39 +0800

    Added lightbeam pages to sitemap.

--- bedrock/urls.py
+++ bedrock/urls.py
@@ -52,7 +52,7 @@ sitemaps = {
          '/newsletter/2012-11/', '/newsletter/2012-10/', '/newsletter/2012-09/', '/newsletter/2012-08/', ],
         0.4, 'monthly'),
     'misc': ViewsSitemap(
-        ['mocotw.news', 'mocotw.sumo', 'newsletter.index'],
+        ['mocotw.news', 'mocotw.sumo', 'newsletter.index', 'lightbeam.lightbeam', 'lightbeam.about'],
         0.4, 'monthly'),
 }
 
