61a6451f1afa02f6d7112a9630eda80184ce81b1
Eddie Lin <elin@mozilla.com>
2014-01-23 17:36:39 +0800

    Expanded click range for spring couplets.

--- media/js/mocotw/2013review.js
+++ media/js/mocotw/2013review.js
@@ -8,4 +8,9 @@
         $video.attr('src', '//www.youtube.com/embed/2JC7bS5Nlrs?rel=0&autoplay=1');
         $video.show();
     });
+    $('#spring-couplets .spring').click(function(e) {
+        if (e.target==this) {
+            window.location.href = $(this).attr('data-href');
+        }
+    });
 })();
