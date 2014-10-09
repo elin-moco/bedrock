d99566388e97371aac97092f15d21695c7680e66
Eddie Lin <elin@mozilla.com>
2014-10-09 18:25:56 +0800

    Added kwicks to display all recommended addons.

--- media/js/mocotw/10years-firefox-family.js
+++ media/js/mocotw/10years-firefox-family.js
@@ -4,4 +4,10 @@ $(function(){
     $('#firefox-family-intro').on('ended', function() {
         $('#coming-soon').addClass('appear');
     });
+    $('#all-addons .kwicks').kwicks({
+        spacing: 0,
+        maxSize: 640,
+        behavior: 'menu',
+        easing: 'easeInOut'
+    });
 });
